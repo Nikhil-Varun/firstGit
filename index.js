2953c0014082b7925075679c117d0ed0d0b419f7
nikhil varun <nikhilvarun0@gmail.com>
2024-03-11 13:14:00 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Write your code below:
+const heading = document.getElementById("main-heading");
+heading.innerHTML = "Fruit World";
+heading.style.color = 'orange';
+const header = document.getElementById("header");
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const basket = document.getElementById('basket-heading');
+basket.style.color = 'green';
+
+const para = document.getElementById("thanks");
+para.innerHTML = '<p>Please visit us again</p>';
+
